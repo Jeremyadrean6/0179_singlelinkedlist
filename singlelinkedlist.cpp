ae9fd1ccb46ff48fdb3554d44913eb6676047276
Jeremyadrean6 <bangje43121@gmail.com>
2025-05-08 11:23:11 +0700

    membuat singlelinkedlist

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -112,6 +112,28 @@ class list {
     }
 };
 
+int main()
+{
+    singlelinkedlist mhs;
+    int nim;
+    int ch;
+    while (1)
+    {
+        cout << endl
+             << "menu";
+        cout << endl
+             << "1. menambahkan data kedalam list" << endl;
+        cout << "2. menghapus data dari dalam list"<< endl;
+        cout << "3. menampilkan semua data didalam list"<< endl;
+        cout << "4. mencari data dalam list"<< endl;
+        cout << "5. keluar"<< endl;
+        cout << endl;
+             << "masukan pilihan (1-5): ";
+        cin >> ch;  
+ 
+    }
+
+}
 
 
 
